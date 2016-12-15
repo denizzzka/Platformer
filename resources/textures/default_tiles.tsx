e147dfd52aff9d153a4fef896bc3b7c7addaa6b6
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-12-15 20:01:40 +0700

    tiled map editor files removed

--- resources/textures/default_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="default_tiles" tilewidth="18" tileheight="18" tilecount="392" columns="28">
- <image source="BLOCKS.png" width="512" height="256"/>
-</tileset>
