4d60cec8b700f694997680af39bf0e4c581f6b9f
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-12-15 17:05:28 +0700

    reading map 18x18 sprites

--- source/map.d
+++ source/map.d
@@ -3,24 +3,24 @@ module map;
 import gfm.math.vector: vec2ui;
 import dsfml.graphics;
 import std.exception: enforce;
+import std.conv: to;
 
 struct Layer
 {
     vec2ui tileSize;
     float opacity;
-    Sprite[] tiles;
+    ushort[] spriteNumbers;
 }
 
 class Map
 {
     const string fileName;
-    Layer[] Layers;
+    Layer[] layers;
     Texture[] tilesets;
+    Sprite[] tileSprites;
 
     this(string mapName)
     {
-        import std.path;
-
         this.fileName = "resources/maps/"~mapName~".json";
 
         auto j = loadJsonDocument(fileName);
@@ -30,9 +30,57 @@ class Map
             if("image" in ts)
             {
                 string path = "resources/maps/test_map/"~ts["image"].str;
-                tilesets ~= new Texture;
 
-                enforce(tilesets[$-1].loadFromFile(path));
+                auto tileset = new Texture;
+                enforce(tileset.loadFromFile(path));
+
+                int tileWidth = ts["tilewidth"].integer.to!int;
+                int tileHeight = ts["tileheight"].integer.to!int;
+
+                enforce(tileWidth > 0);
+                enforce(tileHeight > 0);
+
+                size_t columns = tileset.getSize.x / tileWidth;
+                size_t rows = tileset.getSize.y / tileHeight;
+
+                enforce(columns > 0);
+                enforce(rows > 0);
+
+                foreach(y; 0 .. rows)
+                {
+                    foreach(x; 0 .. columns)
+                    {
+                        import std.conv: to;
+
+                        IntRect rect;
+
+                        rect.top = (y * tileHeight).to!int;
+                        rect.left = (x * tileWidth).to!int;
+
+                        rect.height = tileHeight;
+                        rect.width = tileWidth;
+
+                        tileSprites ~= new Sprite(tileset);
+                        tileSprites[$-1].textureRect = rect;
+                    }
+                }
+
+                tilesets ~= tileset;
+            }
+        }
+
+        foreach(l; j["layers"].array)
+        {
+            if("data" in l)
+            {
+                Layer layer;
+
+                foreach(d; l["data"].array)
+                {
+                    layer.spriteNumbers ~= d.integer.to!ushort;
+                }
+
+                layers ~= layer;
             }
         }
 
