81749ff28a3076ba8d60ffc4f834076af12ce190
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-12-19 11:52:17 +0700

    struct Keyframe added

--- source/skeleton.d
+++ source/skeleton.d
@@ -19,10 +19,25 @@ struct Slot
     Bone* bone;
 }
 
+enum CurveType
+{
+    LINEAR,
+    STEPPED
+}
+
+struct Keyframe
+{
+    float time;
+    float rotate;
+    vec2f translate;
+    CurveType curveType = CurveType.LINEAR;
+}
+
 class Skeleton
 {
     Bone root;
     Slot[] slots;
+    Keyframe[] keyframes;
 
     this(string fileName)
     {
@@ -30,47 +45,53 @@ class Skeleton
 
         Bone*[string] bonesNames;
         Slot*[string] slotsNames;
+        Keyframe*[string] keyframesNames;
 
         auto json = fileName.readText.parseJSON;
 
+        foreach(i, j; json["bones"].array)
         {
+            enforce(!(i == 0 && ("parent" in j)), "root must not contain parent");
 
-            foreach(i, j; json["bones"].array)
+            Bone* b;
+
+            if(i)
             {
-                enforce(!(i == 0 && ("parent" in j)), "root must not contain parent");
-
-                Bone* b;
-
-                if(i)
-                {
-                    Bone* parent = *(j["parent"].str in bonesNames);
-                    parent.children.length++;
-                    b = &parent.children[$-1];
-                }
-                else
-                {
-                    b = &root;
-                }
-
-                b.rotation = j.optionalJson("rotation", 0);
-                b.coords.x = j.optionalJson("x", 0);
-                b.coords.y = j.optionalJson("y", 0);
-                b.scale.x = j.optionalJson("scaleX", 1);
-                b.scale.y = j.optionalJson("scaleY", 1);
-                debug b.length = j.optionalJson("length", 0);
-
-                bonesNames[j["name"].str] = b;
+                Bone* parent = *(j["parent"].str in bonesNames);
+                parent.children.length++;
+                b = &parent.children[$-1];
             }
+            else
+            {
+                b = &root;
+            }
+
+            b.rotation = j.optionalJson("rotation", 0);
+            b.coords.x = j.optionalJson("x", 0);
+            b.coords.y = j.optionalJson("y", 0);
+            b.scale.x = j.optionalJson("scaleX", 1);
+            b.scale.y = j.optionalJson("scaleY", 1);
+            debug b.length = j.optionalJson("length", 0);
+
+            bonesNames[j["name"].str] = b;
         }
 
+        foreach(i, j; json["slots"].array)
         {
-            foreach(i, j; json["slots"].array)
+            slots.length++;
+            slotsNames[j["name"].str] = &slots[$-1];
+
+            slots[$-1].bone = bonesNames[j["bone"].str];
+        }
+
+        foreach(keyName, j; json["animations"].object)
+        {
+            keyframes.length++;
+            Keyframe* k = &keyframes[$-1];
+            keyframesNames[keyName] = k;
+
+            foreach(boneName, boneJson; j["bones"].object)
             {
-                slots.length++;
-                slotsNames[j["name"].str] = &slots[$-1];
-
-                slots[$-1].bone = bonesNames[j["bone"].str];
-
             }
         }
     }
