9c1ee1475618d2ab8a6b40c14316043875545e5d
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-12-19 10:41:56 +0700

    braces for bones reading code

--- source/skeleton.d
+++ source/skeleton.d
@@ -22,35 +22,37 @@ class Skeleton
     {
         import std.file: readText;
 
-        Bone*[string] bonesNames;
-
         auto json = fileName.readText.parseJSON;
 
-        foreach(i, j; json["bones"].array)
         {
-            enforce(!(i == 0 && ("parent" in j)), "root must not contain parent");
+            Bone*[string] bonesNames;
 
-            Bone* b;
-
-            if(i)
+            foreach(i, j; json["bones"].array)
             {
-                Bone* parent = *(j["parent"].str in bonesNames);
-                parent.children.length++;
-                b = &parent.children[$-1];
-            }
-            else
-            {
-                b = &root;
-            }
+                enforce(!(i == 0 && ("parent" in j)), "root must not contain parent");
 
-            b.rotation = j.optionalJson("rotation", 0);
-            b.coords.x = j.optionalJson("x", 0);
-            b.coords.y = j.optionalJson("y", 0);
-            b.scale.x = j.optionalJson("scaleX", 1);
-            b.scale.y = j.optionalJson("scaleY", 1);
-            debug b.length = j.optionalJson("length", 0);
+                Bone* b;
 
-            bonesNames[j["name"].str] = b;
+                if(i)
+                {
+                    Bone* parent = *(j["parent"].str in bonesNames);
+                    parent.children.length++;
+                    b = &parent.children[$-1];
+                }
+                else
+                {
+                    b = &root;
+                }
+
+                b.rotation = j.optionalJson("rotation", 0);
+                b.coords.x = j.optionalJson("x", 0);
+                b.coords.y = j.optionalJson("y", 0);
+                b.scale.x = j.optionalJson("scaleX", 1);
+                b.scale.y = j.optionalJson("scaleY", 1);
+                debug b.length = j.optionalJson("length", 0);
+
+                bonesNames[j["name"].str] = b;
+            }
         }
     }
 }
