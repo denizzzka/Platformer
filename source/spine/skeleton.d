61ea2a4d8eaeb807a8022de83898a8d7912d4ecf
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-12-20 19:13:22 +0700

    animation data load

--- source/spine/skeleton.d
+++ source/spine/skeleton.d
@@ -6,6 +6,8 @@ import std.string: toStringz;
 class Skeleton
 {
     private spSkeleton* skeleton;
+    private spAnimationState* state;
+    private spAnimationStateData* stateData;
 
     this(string filename, Atlas atlas, float scale)
     {
@@ -14,11 +16,16 @@ class Skeleton
         assert(data);
         spSkeletonJson_dispose(json);
         skeleton = spSkeleton_create(data);
+
+        stateData = spAnimationStateData_create(data);
+        state = spAnimationState_create(stateData);
     }
 
     ~this()
     {
         spSkeleton_dispose(skeleton);
+        spAnimationState_dispose(state);
+        spAnimationStateData_dispose(stateData);
     }
 }
 
@@ -38,3 +45,14 @@ spSkeletonData* spSkeletonJson_readSkeletonDataFile(spSkeletonJson*, const(char)
 spSkeleton* spSkeleton_create (spSkeletonData* data);
 
 void spSkeleton_dispose (spSkeleton* self);
+
+struct spAnimationStateData;
+
+struct spAnimationState;
+
+spAnimationStateData* spAnimationStateData_create (spSkeletonData* skeletonData);
+void spAnimationStateData_dispose (spAnimationStateData* self);
+
+/* @param data May be 0 for no mixing. */
+spAnimationState* spAnimationState_create (spAnimationStateData* data);
+void spAnimationState_dispose (spAnimationState* self);
