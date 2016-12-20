d59cdd6ac78990b1e681ceb33430ff8f79db4ea1
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-12-20 21:06:07 +0700

    SkeletonInstanceDrawable added

--- source/spine/sfml.d
+++ source/spine/sfml.d
@@ -1,7 +1,45 @@
 module spine.sfml;
 
 import spine.atlas;
+import spine.skeleton;
 import dsfml.graphics;
+import dsfml.graphics.drawable;
+
+class SkeletonInstanceDrawable : Drawable
+{
+    SkeletonInstance skeleton;
+    alias skeleton this;
+
+    this(SkeletonInstance si)
+    {
+        skeleton = si;
+    }
+
+    void draw(RenderTarget renderTarget, RenderStates renderStates)
+    {
+    }
+}
+
+SkeletonInstanceDrawable createDrawableInstance(SkeletonData sd) @property
+{
+    return new SkeletonInstanceDrawable(sd.createInstance);
+}
+
+unittest
+{
+    import spine.atlas;
+    import spine.skeleton;
+
+    auto a = new Atlas("resources/textures/GAME.atlas");
+    auto sd = new SkeletonData("resources/animations/actor_pretty.json", a, 1);
+    auto si1 = sd.createInstance;
+    auto si2 = sd.createDrawableInstance;
+
+    destroy(a);
+    destroy(sd);
+    destroy(si1);
+    destroy(si2);
+}
 
 private extern(C):
 
@@ -22,18 +60,4 @@ void _spAtlasPage_disposeTexture(spAtlasPage* self)
 {
     Texture t = cast(Texture) self.rendererObject;
     destroy(t);
-}
-
-unittest
-{
-    import spine.atlas;
-    import spine.skeleton;
-
-    auto a = new Atlas("resources/textures/GAME.atlas");
-    auto sd = new SkeletonData("resources/animations/actor_pretty.json", a, 1);
-    auto si = sd.createInstance();
-
-    destroy(a);
-    destroy(sd);
-    destroy(si);
 }
