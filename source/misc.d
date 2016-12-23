9c7393c5005670539be12d29674cd04fb8d0f779
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-12-23 11:02:37 +0700

    unused std.json code removed

--- source/misc.d
+++ source/misc.d
@@ -2,7 +2,6 @@ module misc;
 
 import dsfml.graphics;
 import std.exception: enforce;
-import std.json;
 import vibe.data.json;
 import std.conv: to;
 
@@ -12,28 +11,6 @@ Texture loadTexture(string path)
     enforce(tileset.loadFromFile(path));
 
     return tileset;
-}
-
-/// Need because TME or JSON library isn't respects JSON float convention
-float getFloatFromJson(inout JSONValue j, string fieldName, float defaultValue)
-{
-    auto json = (fieldName in j);
-
-    if(json is null)
-        return defaultValue;
-    else
-        return getFloatFromJson(json);
-}
-
-/// ditto
-float getFloatFromJson(T)(inout T json)
-if(is(T == JSONValue) || is(T == JSONValue*))
-{
-    if(json.type == JSON_TYPE.FLOAT)
-        return json.floating;
-    else if(json.type == JSON_TYPE.INTEGER)
-        return json.integer.to!float;
-    else assert(0);
 }
 
 /// ditto
