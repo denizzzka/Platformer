a6d39782b705b64f2c34dfaf445fba23eb148e54
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-01-02 22:14:22 +0700

    class Scene added

--- source/scene.d
+++ source/scene.d
@@ -2,6 +2,12 @@ module scene;
 
 import spine.atlas;
 import spine.dsfml;
+import dsfml.graphics: RenderTarget, RenderStates, RenderWindow;
+import dsfml.system.clock;
+import map;
+import math: vec2f;
+import core.time: to;
+import std.conv: to;
 
 static Atlas atlas()
 {
@@ -15,4 +21,43 @@ static Atlas atlas()
     }
 
     return atlas;
+}
+
+interface SceneObject
+{
+    void update(float dt);
+    void draw(RenderTarget renderTarget, RenderStates renderStates);
+}
+
+class Scene
+{
+    private Map sceneMap;
+    private SceneObject[] objects;
+    private Clock frameClock;
+    vec2f currViewPosition = vec2f(0, 0);
+
+    this(string mapFilePath)
+    {
+        sceneMap = new Map(mapFilePath);
+        frameClock = new Clock();
+    }
+
+    void update()
+    {
+        TickDuration td = frameClock.restart.to!TickDuration;
+
+        foreach(ref o; objects)
+            o.update(td.to!("seconds", float));
+    }
+
+    private void drawUnitsOnMapCallback()
+    {
+        foreach(ref o; objects){}
+            //~ o.draw(renderTarget, renderStates);
+    }
+
+    void draw(RenderWindow wnd, RenderStates renderStates, vec2f corner)
+    {
+        sceneMap.draw(wnd, corner);
+    }
 }
