5445edae041a9170842ad805c25694c98da6a766
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-01-03 16:11:32 +0700

    Bullets.update() should work

--- source/bullets/storage.d
+++ source/bullets/storage.d
@@ -1,17 +1,27 @@
 module bullets.storage;
 
 import scene: SceneObject;
+import map: Map;
 import bullets.bullet;
 import dsfml.graphics;
 
 class Bullets: SceneObject
 {
+    private Map _map;
+
     Bullet[] bullets;
+
+    this(Map m)
+    {
+        _map = m;
+    }
 
     void update(float dt)
     {
-        //~ foreach(ref b; bullets)
-            //~ b.doMotion(renderTarget, renderStates);
+        const float g_force = 1200.0f; // FIXME: это нужно хранить в сцене
+
+        foreach(ref b; bullets)
+            b.doMotion(_map, dt, g_force);
     }
 
     void draw(RenderTarget renderTarget, RenderStates renderStates)
