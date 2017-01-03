6227af4c2c6498af1455b51acc48db02f9fdf0f4
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-01-03 15:22:25 +0700

    Bullet.draw added

--- source/bullet.d
+++ source/bullet.d
@@ -2,6 +2,7 @@ module bullet;
 
 import math;
 import map;
+import dsfml.graphics;
 
 struct Bullet
 {
@@ -19,5 +20,16 @@ void doMotion(Map m, Bullet b, float dt, float g_force)
         speed *= windage;
 
         speed.y += g_force * dt;
+    }
+}
+
+void draw(Bullet b, RenderTarget renderTarget, RenderStates renderStates)
+{
+    with(b)
+    {
+        Vertex start = position.gfm_dsfml;
+        Vertex end = (position + speed).gfm_dsfml;
+
+        renderTarget.draw([start, end], PrimitiveType.Lines, renderStates);
     }
 }
