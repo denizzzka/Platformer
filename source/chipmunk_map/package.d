028c2710d72204bc654c99a3d3f959ee138594e9
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-01-04 13:15:49 +0700

    isBulletproof added

--- source/chipmunk_map/package.d
+++ source/chipmunk_map/package.d
@@ -6,14 +6,30 @@ import dchip.all;
 
 class ChipmunkMap
 {
+    private cpSpace space;
+
     this(Map m)
     {
+        space.staticBody = cpBodyNewStatic;
+
         auto l = m.physLayer;
 
         foreach(y; 0 .. l.layerSize.y)
+        {
             foreach(x; 0 .. l.layerSize.x)
             {
-                //~ l.getTileByCoords(vec2i(x, y));
+                if(l.getTileByCoords(vec2i(x, y)).isBulletproof)
+                {
+                }
             }
+        }
     }
+}
+
+private bool isBulletproof(PhysLayer.TileType t) pure
+{
+    return  t == PhysLayer.TileType.OneWay ||
+            t == PhysLayer.TileType.Block ||
+            t == PhysLayer.TileType.SlopeLeft ||
+            t == PhysLayer.TileType.SlopeRight;
 }
