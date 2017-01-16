12bf00d235e976817806f96b719995550c6d35bf
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-01-16 08:31:16 +0700

    skeleton bounds intersection check methods added

--- source/spine/skeleton_bounds.d
+++ source/spine/skeleton_bounds.d
@@ -25,6 +25,19 @@ class SkeletonBounds
     float minY() const { return sp_skeletonBounds.minY; }
     float maxX() const { return sp_skeletonBounds.maxX; }
     float maxY() const { return sp_skeletonBounds.maxY; }
+
+    /** Returns true if the axis aligned bounding box intersects the line segment. */
+    bool aabbIntersectsSegment(float x1, float y1, float x2, float y2)
+    {
+        return spSkeletonBounds_aabbIntersectsSegment(sp_skeletonBounds, x1, y1, x2, y2) != 0;
+    }
+
+    /** Returns the first bounding box attachment that contains the line segment, or null. When doing many checks, it is usually
+     * more efficient to only call this method if spSkeletonBounds_aabbIntersectsSegment returns true. */
+    spBoundingBoxAttachment* intersectsSegment(float x1, float y1, float x2, float y2)
+    {
+        return spSkeletonBounds_intersectsSegment(sp_skeletonBounds, x1, y1, x2, y2);
+    }
 }
 
 private extern(C):
@@ -44,3 +57,6 @@ struct spPolygon;
 spSkeletonBounds* spSkeletonBounds_create ();
 void spSkeletonBounds_dispose (spSkeletonBounds* self);
 void spSkeletonBounds_update (spSkeletonBounds* self, spSkeleton* skeleton, int/*bool*/updateAabb);
+
+int/*bool*/spSkeletonBounds_aabbIntersectsSegment (spSkeletonBounds* self, float x1, float y1, float x2, float y2);
+spBoundingBoxAttachment* spSkeletonBounds_intersectsSegment (spSkeletonBounds* self, float x1, float y1, float x2, float y2);
