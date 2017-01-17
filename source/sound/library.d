3d43097aeaf5905c146bc0a34f0c7704ba423573
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-01-17 16:53:04 +0700

    sound file loading check added

--- source/sound/library.d
+++ source/sound/library.d
@@ -2,13 +2,15 @@ module sound.library;
 
 import dsfml.audio;
 public import dsfml.audio.sound;
+import std.exception: enforce;
 
 private SoundBuffer[] soundBuffers;
 
 Sound loadSound(string name)
 {
     SoundBuffer b = new SoundBuffer;
-    b.loadFromFile(name);
+
+    enforce(b.loadFromFile(name), "Sound "~name~" is not found");
 
     soundBuffers ~= b;
 
